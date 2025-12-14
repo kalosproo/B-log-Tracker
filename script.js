1c2054ccffa958f42a5bdb5724ddd624fbaaf67b
kalosproo <muttukururahul@gmail.com>
2025-12-14 05:32:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ function startCamera(type) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(stream => {
       document.getElementById("video").srcObject = stream;
-      setTimeout(capturePhoto, 3000);
+      setTimeout(capturePhoto, 2000);
     });
 }
 
 function capturePhoto() {
-  let video = document.getElementById("video");
-  let canvas = document.getElementById("canvas");
+  const video = document.getElementById("video");
+  const canvas = document.getElementById("canvas");
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   canvas.getContext("2d").drawImage(video, 0, 0);
-  let time = new Date().toLocaleString();
+
+  const time = new Date().toLocaleString();
   document.getElementById("log").innerText =
-    actionType + " recorded at " + time;
+    `${actionType} recorded at ${time}`;
 }
