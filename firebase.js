1eab2e69fe8ed9a96139b2a6540ab34236ef8b27
kalosproo <muttukururahul@gmail.com>
2025-12-17 15:28:06 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,11 +2,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyCTAj_lXQrV6qvATrLnvg1o62Y4G5QeBmM",
   authDomain: "b-log-tracker-0101.firebaseapp.com",
   projectId: "b-log-tracker-0101",
-  storageBucket: "b-log-tracker-0101.appspot.com",
   messagingSenderId: "230746196644",
-  appId: "1:230746196644:web:a76ade1f41bfc5b83bf431",
+  appId: "1:230746196644:web:a76ade1f41bfc5b83bf431"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const storage = firebase.storage();
